56339dbfdd0469b847c59b0a80df18413e8bb722
Andrey Bulanov <bulanov1804@gmail.com>
2021-09-29 23:20:10 +0300

    Update Modal.jsx

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,44 +1,76 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Overlay, ContentModal } from './Modal.Styled';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export default function Modal ({src, alt, onSelect}) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+       window.removeEventListener('keydown', handleKeyDown);
+    }
+  })
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onSelect();
+      onSelect();
     }
   };
 
-  handleBackDropClick = e => {
+  const handleBackDropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onSelect();
+      onSelect();
     }
   };
-
-  render() {
-    const { src, alt} = this.props;
-    const { handleBackDropClick } = this;
-    return createPortal(
-      <>
-        <Overlay onClick={handleBackDropClick}>
-          <ContentModal>
-            <img src={src} alt={alt} />
-          </ContentModal>
-        </Overlay>
-      </>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <>
+      <Overlay onClick={handleBackDropClick}>
+        <ContentModal>
+          <img src={src} alt={alt} />
+        </ContentModal>
+      </Overlay>
+    </>,
+    modalRoot,
+  );
 }
 
-export default Modal;
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onSelect();
+//     }
+//   };
+
+//   handleBackDropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onSelect();
+//     }
+//   };
+
+//   render() {
+//     const { src, alt} = this.props;
+//     const { handleBackDropClick } = this;
+//     return createPortal(
+//       <>
+//         <Overlay onClick={handleBackDropClick}>
+//           <ContentModal>
+//             <img src={src} alt={alt} />
+//           </ContentModal>
+//         </Overlay>
+//       </>,
+//       modalRoot,
+//     );
+//   }
+// }
+
+// export default Modal;
